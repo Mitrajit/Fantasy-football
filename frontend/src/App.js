98274cb4f20aa4e88b155c058f6e789f4bd98a20
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2022-06-17 12:53:56 +0530

    frontend packages set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Topsection from './components/Topsection';
-import Table from './components/Table';
+import { DashboardCustomizeTwoTone } from '@mui/icons-material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 function App() {
   return (
     <>
+    <ThemeProvider theme={createTheme({ palette: { mode: 'dark' } })}>
     <Navbar />
     <Topsection />
-    <Table />
+    </ThemeProvider>
     </>
   );
 }
 document.body.style.backgroundColor='#191919';
+document.body.style.color='#ffffff';
+
 export default App;
