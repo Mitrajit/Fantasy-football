970d8356c0d86d6a6335894ae571a1c4577fb5e2
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2022-06-20 05:12:02 +0530

    Deployment complete

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
+const path = require('path');
 const operatorRouter = require('./routes/operator');
 const playersRouter = require('./routes/players');
+const playerImageRouter = require('./routes/playerImage');
 require('./services/mongoDB');
 
 app.use('/', operatorRouter);
 app.use('/players', playersRouter);
+app.use('/playerImage', playerImageRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('Server is Running! 🚀');
+  });
+}
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server started\nURL: http://127.0.0.1:${process.env.PORT || 5000}/`); // eslint-disable-line no-console
